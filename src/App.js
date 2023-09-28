26d566559b0ce512076a8d8c56b5fe4f3c6f47ef
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-09-28 19:39:48 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Box from './components/Box';
 
 function App() {
+  const [boxNum, setBoxNum] = useState(0);
+  const [renderedBoxes, setRenderedBoxes] = useState([]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    renderBoxes();
+  };
+
+  const boxNumHandler = (event) => {
+    setBoxNum(parseInt(event.target.value, 10));
+  };
+
+  const renderBoxes = () => {
+    const boxes = [];
+    for (let i = 0; i < boxNum; i++) {
+      boxes.push(<Box key={i} />);
+    }
+    setRenderedBoxes(boxes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="boxNumInput">Number of boxes</label>
+        <input
+          type="number"
+          id="boxNumInput"
+          value={boxNum}
+          onChange={boxNumHandler}
+        />
+        <button type="submit">Enter</button>
+      </form>
+      <div className="box-list">
+        {renderedBoxes}
+      </div>
     </div>
   );
 }
---
+++ src/components/Box.js
@@ -0,0 +1,10 @@
+import './Box.css'
+
+
+function Box(props) {
+    // const classes = "card " + props.className
+
+    return <div className="box"/>
+}
+
+export default Box
